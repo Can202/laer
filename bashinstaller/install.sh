7e136562be01f45cefe1b7d1cea5088a0607f9fc
Can202 <mgoopazo@gmail.com>
2021-09-05 17:25:00 -0300

    fix bashinstaller

--- bashinstaller/install.sh
+++ bashinstaller/install.sh
@@ -59,52 +59,11 @@ mkdir laer
 
 cd laer
 
-$SUDO curl -OL https://github.com/Can202/laer/releases/download/v0.2/laer
+$SUDO curl -OL https://raw.githubusercontent.com/Can202/laer/v0.2/laer
+$SUDO curl -OL https://raw.githubusercontent.com/Can202/laer/v0.2/bashinstaller/laer-delete
 
 
 
-echo "
-#!/bin/bash
-
-SUDO=\"\"
-if [ ! $(whoami) = \"root\" ]
-then
-	echo \"you have to run this script as root\"
-	
-	echo
-	echo \"Do you wanna try with sudo? (y/n)\"
-	read try
-	if [ $try = \"y\" ] || [ $try = \"Y\" ]
-	then
-		echo \"ok!\"
-		SUDO=\"sudo\"
-	else
-		echo \"aborting\"
-		exit
-	fi
-fi
-
-if [ -f \"/etc/laer/linked\" ]
-then
-    LINKED=$(cat /etc/laer/linked)
-    COLINKED=$(cat /usr/bin/$LINKED)
-    CORIGINAL=$(cat /usr/bin/laer)
-    COOCORTED=${CORIGINAL:0:25}
-    COLCORTED=${COLINKED:0:25}
-    
-	if [[ $COOCORTED = $COLCORTED ]]
-	then
-        echo \"You've been created a link\" 
-		$SUDO rm -f /usr/bin/$LINKED
-		echo \"link removed\"
-	fi
-	$SUDO rm -f /etc/laer/linked
-	echo \"link name removed\"
-fi
-
-$SUDO rm -f /usr/bin/laer
-$SUDO rm -f /usr/bin/laer-delete
-" > laer-delete
 $SUDO chmod a+x laer-delete
 $SUDO chmod a+x laer
 
